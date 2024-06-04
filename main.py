caa5dad8da395329e6014e8e4bdd489d613cae40
LukeZHar <15649@coderacademy.edu.au>
2024-06-04 20:31:34 +1000

    Data types

--- main.py
+++ main.py
@@ -1,3 +1,57 @@
-print("Hello World!!")
-print(2 * 20)
-print("Whats 2 + 2 =", 2 + 2)
+# print("Hello World!!")
+# print(2 * 20)
+# print("Whats 2 + 2 =", 2 + 2)
+
+# print("It's a comment,it will be ignored")
+
+# Data types
+# Integers
+# 4
+# 7
+# 9
+# print(9)
+# print("9")
+# print(type(9))
+# print(type("9"))
+
+# # floats
+# 2.5
+# 5.123123
+# print(3.1415)
+# print(type(-5.63))
+# print(type(-5))
+
+# # Complex (imaginary numbers), No worries
+# 2 + 3
+# print(type(4 +7j))
+
+# Operators + - * / %
+print(4 * 5)
+print(15 / 3)
+print(15 / 2)
+print(7 % 2)
+
+# String
+"Hello world"
+'Hello'
+print("This is a string")
+
+# String concatenations, addition, join
+print("Hello" + " World")
+print(3 + 5)
+print("3" + "5")
+
+#print(bool(""))
+#print(bool(-123))
+#print(bool("False"))
+
+# Comparison operators <, >, <=, >=, ==, !=
+print(2 < 3)
+print(5 >= 5)
+print("HELLO".lower)
+print("Hello".lower() == "hello")
+
+# Logical operators and, or, not
+print(True and False) # AND returns True when both operators are True
+print(False or True) # OR returns True, when one or both of the operators are True
+print(not False) #NOT returns the opposite value
